f795f9b956dc752d5eb386aa556904cacc58e6c7
AyumuShigeta <faliplvsg@gmail.com>
2023-10-19 01:29:36 +0900

    アクター追加

---
+++ src/conf/authority.go
@@ -0,0 +1,8 @@
+package conf
+
+const (
+	CustomUserClaimAdmin        = "admin"         // システム管理者
+	CustomUserClaimNurseryAdmin = "nursery-admin" // 保育園上長職員
+	CustomUserClaimNurseryUser  = "nursery-user"  //  保育園職員
+	CustomUserClaimParentUser   = "parent-user"   // 保護者
+)
